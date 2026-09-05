f31c596255710630fd3b9bd88c69225dfa35f6e0
Naiara Cassimiro <ncassimiro@byupathway.edu>
2026-09-05 14:34:06 -0300

    Semana 01

---
+++ scripts/course.js
@@ -0,0 +1,62 @@
+const courses = [
+    {
+        subject: 'CSE',
+        number: 110,
+        title: 'Introduction to Programming',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce students to programming.',
+        technology: ['Python'],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 130,
+        title: 'Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course introduces students to the World Wide Web.',
+        technology: ['HTML', 'CSS'],
+        completed: true
+    },
+    {
+        subject: 'CSE',
+        number: 111,
+        title: 'Programming with Functions',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course introduces students to programming with functions.',
+        technology: ['Python'],
+        completed: true
+    },
+    {
+        subject: 'CSE',
+        number: 210,
+        title: 'Programming with Classes',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course introduces students to programming with classes.',
+        technology: ['C#'],
+        completed: false
+    },
+    {
+        subject: 'WDD',
+        number: 131,
+        title: 'Dynamic Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience in Web fundamentals.',
+        technology: ['HTML', 'CSS', 'JavaScript'],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 231,
+        title: 'Web Frontend Development I',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course focuses on frontend Web development.',
+        technology: ['HTML', 'CSS', 'JavaScript'],
+        completed: false
+    }
+];
