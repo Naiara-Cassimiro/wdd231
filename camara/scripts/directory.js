89dc750b496fe8f56d4c06cf1ccdfb45495553ea
Naiara Cassimiro <ncassimiro@byupathway.edu>
2026-09-07 18:59:53 -0300

    Semana 02

--- camara/scripts/directory.js
+++ camara/scripts/directory.js
@@ -1,6 +1,5 @@
 const url = "data/members.json";
 const membersContainer = document.querySelector("#members");
-
 const gridButton = document.querySelector("#grid");
 const listButton = document.querySelector("#list");
 
@@ -49,7 +48,6 @@ function displayMembers(members) {
         card.appendChild(name);
         card.appendChild(address);
         card.appendChild(phone);
-        card.appendChild(membership);
         card.appendChild(website);
 
         membersContainer.appendChild(card);
