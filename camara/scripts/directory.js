c137d1715ed9a9c7a6a1f8cbb859622cc2ba03c4
Naiara Cassimiro <ncassimiro@byupathway.edu>
2026-09-07 17:21:01 -0300

    Semana 02

--- camara/scripts/directory.js
+++ camara/scripts/directory.js
@@ -1,10 +1,38 @@
 const url = "data/members.json";
+const membersContainer = document.querySelector("#members");
 
 async function getMembers() {
     const response = await fetch(url);
     const data = await response.json();
 
-    console.log(data.members);
+    displayMembers(data.members);
+}
+
+function displayMembers(members) {
+    members.forEach((member) => {
+        const card = document.createElement("section");
+
+        const name = document.createElement("h2");
+        name.textContent = member.name;
+
+        const address = document.createElement("p");
+        address.textContent = member.address;
+
+        const phone = document.createElement("p");
+        phone.textContent = member.phone;
+
+        const website = document.createElement("a");
+        website.href = member.website;
+        website.textContent = "Visitar site";
+        website.target = "_blank";
+
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+
+        membersContainer.appendChild(card);
+    });
 }
 
 getMembers();
